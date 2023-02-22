99c983e06a6c4389f23b33d90bb7c2395a72f801
Mansi1299 <mansi.joshi@preciousinfosystem.com>
2023-02-22 16:44:48 +0530

    firts

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -5,15 +5,18 @@ function Todo(props) {
   return (
     <div className="todo" >
       {/* <h1 >This is Todo List</h1> */}
-      <div style={{ backgroundColor: 'black', padding: '5px' ,width:"100%" }}>
-        <h1 style={{ textAlign: "center", color: "white" }}>Todo-App</h1></div>
-      <input type="text" className='input-box' placeholder='Enter Anything' style={{ textAlign: 'center', marginTop: "10px", width: "90%" }}
+      <div style={{ backgroundColor: '   #202b35', padding: '3px' ,width:"100%" }}>
+        <h1 style={{ padding:"2px",margin:'15px', color: "white" }}>To Do List <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    99+
+    <span class="visually-hidden">unread messages</span>
+  </span></h1></div>
+      <input type="text" className='input-box' placeholder='Enter Anything' style={{padding:'8px' , margin:"20px", width: "92%" }}
         value={uInput}
         onChange={e => {
           setInput(e.target.value)
         }}
       />
-      <button style={{ backgroundColor: "#3399FF", borderColor: '#3399FF', color: "white", marginLeft: "10px" }} className='btn-primary' onClick={() => {
+      <button style={{ padding:"7px",backgroundColor: "#054988", borderColor: '#3399FF', color: "white", marginLeft: "1px" ,width:'75px'}} className='btn-primary' onClick={() => {
         props.add(uInput)
         setInput("")
       }}>Add Task</button>
--- src/Component/Todolists.js
+++ src/Component/Todolists.js
@@ -2,10 +2,10 @@ import React from 'react'
 const Todolists = (props) => {
   return (
     <div>
-      <li style={{ margin: "10px" }}>
+      <li style={{ margin: "10px",padding:"15px" }}>
         {props.item}
         <span>
-          <i className="fa-solid fa-delete-left" style={{ marginRight: "30px", marginLeft: "15%", float: "right" }} onClick={
+          <i className="fa-solid fa-delete-left" style={{ marginRight: "30px", marginLeft: "15%", float: "right",color :"red",fontSize:"30px" }} onClick={
             e => {
               props.deleteitem(props.index)
             }
